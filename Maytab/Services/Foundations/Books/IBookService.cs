d0ac0c9a69b0b116d7399db5a234276424c52c30
jaloliddin.mahkamov@mail.ru <jaloliddin.mahkamov@mail.ru>
2025-02-20 01:55:10 +0500

    FOUNDATIONS: Book Crud Implement

---
+++ Maytab/Services/Foundations/Books/IBookService.cs
@@ -0,0 +1,12 @@
+﻿using Maytab.Models.Foundations.Books;
+
+namespace Maytab.Services.Foundations.Books
+{
+    public interface IBookService
+    {
+        ValueTask<Book> AddBookAsync(Book book);
+        ValueTask<IQueryable<Book>> GetAllBooksAsync();
+        ValueTask<Book> RetrieveBookByIdAsync(int id); 
+        ValueTask<Book> RemoveBookByIdAsync(int id);
+    }
+}
