160a56cb19ba176d4e626fe21748fb07744b5c8b
jaloliddin.mahkamov@mail.ru <jaloliddin.mahkamov@mail.ru>
2025-02-20 01:13:11 +0500

    INFRA: Maytab Api Project Initialize

---
+++ Maytab/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Maytab.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult<string> Get()=>
+            Ok("Hello, World!");
+
+    }
+}
